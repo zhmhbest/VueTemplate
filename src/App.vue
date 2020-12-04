0e03f9b6802ab6590042607f560e6642b624aa8a
zhmhbest <zhmhbest@gmail.com>
2020-12-04 17:29:08 +0800

    布局

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
     <div id='app'>
-        <div>Top</div>
-        <router-view/>
+        <div class="frame-top">Top</div>
+        <div class="container-col">
+            <div class="frame-left">Left</div>
+            <div class="frame-main"><router-view/></div>
+            <div class="frame-right">Right</div>
+        </div>
+        <div class="frame-bottom">Bottom</div>
     </div>
 </template>
 
@@ -13,6 +18,34 @@
 
 <style lang='scss'>
     #app {
-        background-color: cornflowerblue;
+        .frame-top {
+            display: flex;
+            background-color: cornflowerblue;
+        }
+        .frame-bottom {
+            display: flex;
+            background-color: palevioletred;
+        }
+        .container-col {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+        .frame-left {
+            display: inline-block;
+            width: 10%;
+            background-color: peru;
+        }
+        .frame-main {
+            display: inline-block;
+            width: 80%;
+            background-color: gray;
+        }
+        .frame-right {
+            display: inline-block;
+            width: 10%;
+            background-color: powderblue;
+        }
     }
 </style>
